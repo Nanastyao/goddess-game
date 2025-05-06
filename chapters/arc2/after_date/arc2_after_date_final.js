66ba59b93188f848282410fe7f19faa89415c01d
YorikoSC <perov.sync@ya.ru>
2025-05-06 11:05:54 +0300

    обавлены изменения из Яндекс.Диска

--- chapters/arc2/after_date/arc2_after_date_final.js
+++ chapters/arc2/after_date/arc2_after_date_final.js
@@ -7,7 +7,7 @@ export default {
         const texts = gameState.language === 'en' ? {
             choice1:"Okay, tomorrow then. Text me when you're free.",
             result: "I will! And... maybe I'll send you a couple more photos... especially for you 💋",
-            nextChapter: "arc2/way_to_NTR/look_what_i_have"
+            nextChapter: "arc2_way_to_NTR/look_what_i_have"
         } : {
             choice1:"Хорошо, завтра. Напиши, когда освободишься.",
             result: "Обязательно! И... может быть, пришлю тебе ещё пару фото... специально для тебя 💋",
